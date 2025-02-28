a30cef5be5cba83f1318d60305d5d2f2c0b59604
whostausif <tausifwali@gmail.com>
2025-02-28 19:54:30 +0600

    lesson 6 activity 2

---
+++ activity2.py
@@ -0,0 +1,25 @@
+import turtle
+
+turtle.Screen().bgcolor("green")
+star = turtle.Turtle()
+
+star.forward(200)
+star.left(120)
+star.forward(200)
+star.left(120)
+star.forward(200)
+
+star.penup()
+
+
+star.right(150)
+star.forward(50)
+star.pendown()
+star.right(90)
+star.forward(200)
+star.right(120)
+star.forward(200)
+star.right(120)
+star.forward(200)
+
+turtle.done()
